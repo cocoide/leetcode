3b5a5455d73c204763f12d1008cf86e1ccb5d247
{cocoide} <{cocoide.com@gmail.com}>
2023-03-14 03:49:00 +0900

    Add climbStairs

---
+++ code/easy_70_climbing_stairs.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=70 lang=golang
+ *
+ * [70] Climbing Stairs
+ */
+package code
+
+// @lc code=start
+func climbStairs(n int) int {
+    a, b :=1, 1
+	for ;n>1;n--{
+		a, b=b, a+b
+	}
+	return b
+}
+// use Fibonacci sequence
+// @lc code=end
+
